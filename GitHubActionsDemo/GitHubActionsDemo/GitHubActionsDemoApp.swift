eb732b7d860daa2167a0886aac798350c58e7824
freelancer12exp <freelancer12exp@gmail.com>
2024-08-25 22:29:09 +0530

    initial commit

---
+++ GitHubActionsDemo/GitHubActionsDemo/GitHubActionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsDemoApp.swift
+//  GitHubActionsDemo
+//
+//  Created by Prashamsa on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
